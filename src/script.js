83165d751b86067f85d4fe340ddc7a9b6a12f79a
AbPip <pipergail.abik2@gmail.com>
2025-03-07 00:22:18 +0000

    Made it

---
+++ src/script.js
@@ -0,0 +1,28 @@
+//const tablex = document.getElementById('theTable'),
+//clicked = document.getElementById('coordinates');
+
+//randomColor = () => {
+//  return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+//}
+
+//colorMe = (that) => {
+  //that.style.backgroundColor = randomColor();
+  //clicked.innerHTML = that.parentNode.rowIndex + '/' + that.cellIndex;
+//}
+
+//colorCell = () => {
+  
+  //try {
+  //const xInput = document.getElementById('theX'),
+        //yInput = document.getElementById('theY'),
+        //theCell = tablex.rows[xInput.value].cells[yInput.value];
+        
+    //theCell.style.backgroundColor = randomColor();
+    //clicked.innerHTML = xInput.value + '/' + yInput.value;
+    
+  //} catch {
+    //alert('Check your x/y buddy, there are not that many cells... 0 - 2');
+  //}
+  
+//}
+
